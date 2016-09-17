be9e4ab8465b993fd09a4273fb44a680665ea7bc
XLuoChen <1425145741@qq.com>
2016-09-17 14:27:48 +0800

    improve

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,40 +1,36 @@
 'use strict';
 const processNumber = require('../src/main');
 
-describe('printStudentNumbers', ()=> {
+describe('processNumber', ()=> {
   const specialNumbers = [3, 5, 7];
-  const totalNumber = 100;
 
-  describe('processNumber', ()=> {
-    it('is not special number', ()=> {
-      const number = processNumber(1, specialNumbers);
-      expect(number).toEqual(1);
-    });
-
-    it('times(3)=>Fizz', ()=> {
-      const number = processNumber(9, specialNumbers);
-      expect(number).toEqual('Fizz');
-    });
-
-    it('times(5)=>Buzz', ()=> {
-      const number = processNumber(10, specialNumbers);
-      expect(number).toEqual('Buzz');
-    });
-
-    it('times(7)=>Whizz', ()=> {
-      const number = processNumber(14, specialNumbers);
-      expect(number).toEqual('Whizz');
-    });
-
-    it('times(3,5)=>FizzBuzz', ()=> {
-      const number = processNumber(15, specialNumbers);
-      expect(number).toEqual('FizzBuzz');
-    });
-
-    it('includes(3)=>Fizz', ()=> {
-      const number = processNumber(35, specialNumbers);
-      expect(number).toEqual('Fizz');
-    });
+  it('is not special number', ()=> {
+    const number = processNumber(1, specialNumbers);
+    expect(number).toEqual(1);
   });
 
+  it('times(3)=>Fizz', ()=> {
+    const number = processNumber(9, specialNumbers);
+    expect(number).toEqual('Fizz');
+  });
+
+  it('times(5)=>Buzz', ()=> {
+    const number = processNumber(10, specialNumbers);
+    expect(number).toEqual('Buzz');
+  });
+
+  it('times(7)=>Whizz', ()=> {
+    const number = processNumber(14, specialNumbers);
+    expect(number).toEqual('Whizz');
+  });
+
+  it('times(3,5)=>FizzBuzz', ()=> {
+    const number = processNumber(15, specialNumbers);
+    expect(number).toEqual('FizzBuzz');
+  });
+
+  it('includes(3)=>Fizz', ()=> {
+    const number = processNumber(35, specialNumbers);
+    expect(number).toEqual('Fizz');
+  });
 });
